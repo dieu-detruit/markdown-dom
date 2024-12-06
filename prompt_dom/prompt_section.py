abd4f25c980a27895fa60238bd25c079336a02f2
Watanabe Takafumi <dieu.detruit@gmail.com>
2024-12-06 23:30:53 +0900

    refined impl

--- prompt_dom/prompt_section.py
+++ prompt_dom/prompt_section.py
@@ -1,44 +1,43 @@
 from __future__ import annotations
 
+from typing import TypeVar
 
+import pydantic.dataclasses
+
+from .renderer import BasePromptRenderer
+
+
+@pydantic.dataclasses.dataclass
 class PromptSectionTitle:
-    def __init__(self, title: str):
+    title: str
+
+    def __init__(self, title: str) -> None:
         self._title = title
 
-    @property
-    def title(self) -> str:
-        return self._title
+
+PromptRenderer = TypeVar("PromptRenderer", bound=BasePromptRenderer)
 
 
+@pydantic.dataclasses.dataclass
 class PromptSection:
+    prompt_title: str | None
+    elements: list[PromptSection | str]
+
     def __init__(
         self,
-        prompt_title_or_element: PromptSectionTitle | PromptSection | str,
-        *elements: PromptSection | str,
+        prompt_title_or_element: PromptSectionTitle | PromptSection | str | None,
+        *elements: PromptSection | str | None,
     ):
         if isinstance(prompt_title_or_element, PromptSectionTitle):
-            self._prompt_title: str | None = prompt_title_or_element.title
+            self.prompt_title = prompt_title_or_element.title
+            self.elements = [element for element in elements if element is not None]
         else:
-            self._prompt_title = None
+            self.prompt_title = None
+            self.elements = [
+                element
+                for element in [prompt_title_or_element, *elements]
+                if element is not None
+            ]
 
-        self._elements = elements
-
-    def render(self, *, section_level: int = 2) -> str:
-        rendered_elements = []
-        if self._prompt_title is not None:
-            rendered_elements.append(f"{'#' * section_level} {self._prompt_title}")
-
-        for element in self._elements:
-            if isinstance(element, str):
-                rendered_elements.append(element)
-                continue
-
-            # PromptSection element
-            rendered_elements.append("")
-            rendered_elements.append(element.render(section_level=section_level + 1))
-
-        # remove redundant empty line at the beginning
-        if self._prompt_title is None:
-            rendered_elements.pop(0)
-
-        return "\n".join(rendered_elements)
+    def render(self, *, section_level: int = 2, renderer: PromptRenderer) -> str:
+        return renderer.render_prompt_section(self, section_level=section_level)
